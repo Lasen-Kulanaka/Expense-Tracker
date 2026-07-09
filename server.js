a89b4ccfe0b902511de090ab7bcff608c7330358
Lasen Kulanaka <147847779+Lasen-Kulanaka@users.noreply.github.com>
2026-07-09 22:47:58 +0530

    Backend: auth and expense CRUD complete

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const expenseRoutes = require('./routes/expenses');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/expenses', expenseRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running');
